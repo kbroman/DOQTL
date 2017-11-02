d1ebb658f3d2e976c6698cce78e507a2d5745dab
Karl Broman <kbroman@gmail.com>
2017-11-02 13:05:49 -0500

    Forgot to include <R.h> in filter.smooth.allele.c to go with warning()
     - Plus just messed up position of one of the warnings

--- src/filter.smooth.allele.c
+++ src/filter.smooth.allele.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *  filter.alleles.c
- *  
+ *
  *  Created by Daniel Gatti on 4/09/2013.
  *  Copyright 2013 The Jackson Laboratory. All rights reserved.
  *  int* dims: vector with four values: number of states, number of samples,
@@ -17,12 +17,13 @@
  *  double* prfilt: 3D matrix of filtered log-probabilities. num states *
  *                  num samples * num SNPs.
  * R passes a pointer down for arrays.  We have to index into the 3D arrays by
- * hand.  SNPs are in the first slice.  Each slice has states in rows and 
+ * hand.  SNPs are in the first slice.  Each slice has states in rows and
  * samples in columns.  Since R uses column-ordering for matrices, indexing
  * through snps, then samples, then states should minimize cache-misses.
  */
+#include <R.h>
 #include <R_ext/Print.h>
-#include <R_ext/Utils.h> 
+#include <R_ext/Utils.h>
 #include "addlog.h"
 
 
@@ -46,84 +47,84 @@ void filter_smooth_allele(int* dims, int* geno, double* a, double* b, double* pr
   int b_slice = num_states * num_symbols;  /* One SNP slice in b matrix. */
   int geno_snp_index = 0; /* SNP index into geno array. */
   int geno_sam_index = 0; /* Sample index into geno array. */
-  double sums = -DBL_MAX;  /* Sums for each sample. */  
+  double sums = -DBL_MAX;  /* Sums for each sample. */
   double* prpred = (double*)malloc(num_states * num_samples * num_snps * sizeof(double));
   double* prfilt = (double*)malloc(num_states * num_samples * num_snps * sizeof(double));
-  
+
   /* Initialize filtered probabilities by copying the predictive values at the
    * first SNP into the first SNP of the filtered array. */
   loglik[0] = -DBL_MAX;
   snp = 0;
 
   for(sam = 0; sam < num_samples; sam++) {
-	/* Note that we have to initialize with this to make the addlog values correct.
-	 * exp(0) is 1. exp(-Inf) = 0. */
-	sums = -DBL_MAX;
-	/* pr_snp_index moves us to the current sample here. */
-	pr_sam_index = sam * num_states;
+    /* Note that we have to initialize with this to make the addlog values correct.
+     * exp(0) is 1. exp(-Inf) = 0. */
+    sums = -DBL_MAX;
+    /* pr_snp_index moves us to the current sample here. */
+    pr_sam_index = sam * num_states;
     for(st1 = 0; st1 < num_states; st1++) {
       /* pr_sam_index moves through states in one sample here. */
-	  pr_st_index = st1 + pr_sam_index;
-	  /* Numerator of Eqn. 11 in Churchill, 1989.  */
-	  prpred[pr_st_index] = init[st1];
+      pr_st_index = st1 + pr_sam_index;
+      /* Numerator of Eqn. 11 in Churchill, 1989.  */
+      prpred[pr_st_index] = init[st1];
       if(geno[sam] >= num_symbols) warning("Invalid geno\n");
       prfilt[pr_st_index] = b[st1 * num_symbols + geno[sam]] + prpred[pr_st_index];
-	  loglik[0] = addlog(loglik[0], prfilt[pr_st_index]);
+      loglik[0] = addlog(loglik[0], prfilt[pr_st_index]);
       /* Create the denominator for Eqn. 11 in Churchill, 1989. */
-	  sums = addlog(sums, prfilt[pr_st_index]);
-	} /* for(st1) */
-	
-	/* Normalize this sample by dividing by all states. */
-	for(st1 = 0; st1 < num_states; st1++) {
-	  /* Divide each state by the denominator for Eqn. 11 in Churchill, 1989. */
-	  prfilt[st1 + pr_sam_index] -= sums;
-	} /* for(st1) */
+      sums = addlog(sums, prfilt[pr_st_index]);
+    } /* for(st1) */
+
+    /* Normalize this sample by dividing by all states. */
+    for(st1 = 0; st1 < num_states; st1++) {
+      /* Divide each state by the denominator for Eqn. 11 in Churchill, 1989. */
+      prfilt[st1 + pr_sam_index] -= sums;
+    } /* for(st1) */
   } /* for(sam) */
-  
-  /* Loop through each SNP, filling in the predictive array and then the 
+
+  /* Loop through each SNP, filling in the predictive array and then the
    * filtered array. */
   for(snp = 1; snp < num_snps; snp++) {
-	if(snp % 100 == 0)	R_CheckUserInterrupt();
-	
+    if(snp % 100 == 0)	R_CheckUserInterrupt();
+
     /* The transition matrix array has one less SNP than the probability
      * arrays.  So the index for the transition matrix is the SNP - 1. */
-	/* tr_snp_index moves us to the current SNP in the transition matrix array. */
+    /* tr_snp_index moves us to the current SNP in the transition matrix array. */
     tr_snp_index = (snp - 1) * tr_mat_slice;
     b_index = snp * b_slice;
     geno_snp_index   = snp * num_samples;
     pr_snp_index = snp * pr_mat_slice;
     for(sam = 0; sam < num_samples; sam++) {
       sums = -DBL_MAX;
-      /* pr_sam_index moves us to the current sample and SNP in the probability 
+      /* pr_sam_index moves us to the current sample and SNP in the probability
        * arrays. */
-	  pr_sam_index = sam * num_states + pr_snp_index;
-	  geno_sam_index = geno_snp_index + sam;
-	  for(st1 = 0; st1 < num_states; st1++) {
-		/* pr_st_index moves us to the current state. */ 
+      pr_sam_index = sam * num_states + pr_snp_index;
+      geno_sam_index = geno_snp_index + sam;
+      for(st1 = 0; st1 < num_states; st1++) {
+        /* pr_st_index moves us to the current state. */
         pr_st_index = st1 + pr_sam_index;
-		prpred[pr_st_index] = -DBL_MAX;
-		prfilt[pr_st_index] = -DBL_MAX;
-		for(st2 = 0; st2 < num_states; st2++) {
+        prpred[pr_st_index] = -DBL_MAX;
+        prfilt[pr_st_index] = -DBL_MAX;
+        for(st2 = 0; st2 < num_states; st2++) {
           /* Eqn 10 in Churchill, 1989. */
-		  prpred[pr_st_index] = addlog(prpred[pr_st_index], 
-		                        a[st2 + st1 * num_states + tr_snp_index] +
-							    prfilt[st2 + sam * num_states +
-							    (snp - 1) * pr_mat_slice]);
+          prpred[pr_st_index] = addlog(prpred[pr_st_index],
+                                a[st2 + st1 * num_states + tr_snp_index] +
+                                prfilt[st2 + sam * num_states +
+                                (snp - 1) * pr_mat_slice]);
         } /* for(st2) */
-		/* Numerator in Eqn 11 in Churchill, 1989. */
-        prfilt[pr_st_index] = b[b_index + st1 * num_symbols + geno[geno_sam_index]] + 
+        /* Numerator in Eqn 11 in Churchill, 1989. */
         if(geno[geno_sam_index] >= num_symbols) warning("Invalid geno\n");
+        prfilt[pr_st_index] = b[b_index + st1 * num_symbols + geno[geno_sam_index]] +
                                 prpred[pr_st_index];
         loglik[0] = addlog(loglik[0], prfilt[pr_st_index]);
         sums = addlog(sums, prfilt[pr_st_index]);
-	  } /* for(st1) */
+      } /* for(st1) */
 
       /* Normalize this sample by dividing by all states. */
-	  for(st1 = 0; st1 < num_states; st1++) {
+      for(st1 = 0; st1 < num_states; st1++) {
         /* Divide by the denominator in Eqn 11 in Churchill, 1989. */
-	    prfilt[st1 + pr_sam_index] -= sums;
-	  } /* for(st1) */
-	} /* for(sam) */
+        prfilt[st1 + pr_sam_index] -= sums;
+      } /* for(st1) */
+    } /* for(sam) */
   } /* for(snp) */
 
   /*** Smooth ***/
@@ -137,44 +138,41 @@ void filter_smooth_allele(int* dims, int* geno, double* a, double* b, double* pr
     pr_sam_index = sam * num_states + snp * pr_mat_slice;
     for(st1 = 0; st1 < num_states; st1++) {
       /* pr_st_index moves us to the correct state. */
-	  pr_st_index = st1 + pr_sam_index;
+      pr_st_index = st1 + pr_sam_index;
       prsmth[pr_st_index] = prfilt[pr_st_index];
-	} /* for(st1) */
+    } /* for(st1) */
   } /* for(sam) */
 
   /* Loop backward through all of the SNPs and update the smoothed probabilities. */
   for(snp = num_snps - 2; snp >= 0; snp--) {
-	if(snp % 100 == 0) R_CheckUserInterrupt();
+    if(snp % 100 == 0) R_CheckUserInterrupt();
     /* tr_snp_index moves us to the correct SNP in the transition matrix array. */
-	tr_snp_index = snp * tr_mat_slice;
-	pr_snp_index = (snp + 1) * pr_mat_slice;
+    tr_snp_index = snp * tr_mat_slice;
+    pr_snp_index = (snp + 1) * pr_mat_slice;
     for(sam = 0; sam < num_samples; sam++) {
       /* pr_sam_index moves us to the successive SNP and sample in the probability
        * arrays. */
-	  pr_sam_index = sam * num_states + pr_snp_index;
-	  for(st1 = 0; st1 < num_states; st1++) {
+      pr_sam_index = sam * num_states + pr_snp_index;
+      for(st1 = 0; st1 < num_states; st1++) {
         sums = -DBL_MAX;
-		for(st2 = 0; st2 < num_states; st2++) {
+        for(st2 = 0; st2 < num_states; st2++) {
           /* pr_st_index moves us to the current state. */
-		  pr_st_index = st2 + pr_sam_index;
-		  /* Left summation of Eqn. 13 in Churchill, 1989 */
-		  sums = addlog(sums, a[st1 + st2 * num_states + tr_snp_index] +
-				        prsmth[pr_st_index] - prpred[pr_st_index]);
-		} /* for(st2)*/
-		/* pr_st_index moves us to the current SNP and sample in the probability
-		 * arrays. */
-		pr_st_index = st1 + sam * num_states + snp * pr_mat_slice;
-		/* Multiply by prfilt as in Eqn. 13 of Churchill, 1989. */
-		prsmth[pr_st_index] = prfilt[pr_st_index] + sums;
-	  } /* for(st1) */
-	} /* for(sam) */
+          pr_st_index = st2 + pr_sam_index;
+          /* Left summation of Eqn. 13 in Churchill, 1989 */
+          sums = addlog(sums, a[st1 + st2 * num_states + tr_snp_index] +
+                        prsmth[pr_st_index] - prpred[pr_st_index]);
+        } /* for(st2)*/
+        /* pr_st_index moves us to the current SNP and sample in the probability
+         * arrays. */
+        pr_st_index = st1 + sam * num_states + snp * pr_mat_slice;
+        /* Multiply by prfilt as in Eqn. 13 of Churchill, 1989. */
+        prsmth[pr_st_index] = prfilt[pr_st_index] + sums;
+      } /* for(st1) */
+    } /* for(sam) */
   } /* for(snp) */
 
 
   free(prpred);
   free(prfilt);
-  
+
 } /* filter_smooth_allele() */
-
-
-
